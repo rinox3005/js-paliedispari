7cd1c217285481d9a94582637cb57e5228acc498
rinox3005 <peppe.rinox@gmail.com>
2024-05-07 19:11:51 +0200

    [UPD] user input done with checks for every wrong interaction

---
+++ dispari/js/dispari.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+
+// definisco una variabile con il valore di un prompt che chiede pari o dispari pari/dispari
+
+const userEvenOddChoice = prompt('Do you choose even or odd?');
+
+//  mostro in console e nel documento il contenuto di userChoice
+
+console.log('The user choice for even and odd is: ', userEvenOddChoice);
+
+// inizializzo una variabile userNumberChoice
+
+let userNumberChoice;
+
+// con un ciclo do while chiedo all'utente un numero compreso tra 1 e 5 controllando che non inserisca valori diversi
+
+do {
+    userNumberChoice = Number(prompt('Choose a number between 1 and 5'));
+
+    // controllo che non inserisca qualcosa di diverso da un numero
+    if (isNaN(userNumberChoice)) {
+        alert('ERROR! Enter a number');
+        // controllo che non inserisca un numero decimale
+    } else if (userNumberChoice % 1 !== 0) {
+        alert('ERROR! Enter an integer number');
+        // controllo che non inserisca un numero non compreso tra 1 e 5
+    } else if (userNumberChoice < 1 || userNumberChoice > 5) {
+        alert('ERROR! Enter a valid number');
+    }
+    // finché non rispetta tutte queste condizioni continuo a chiedere
+} while (userNumberChoice < 1 || userNumberChoice > 5 || isNaN(userNumberChoice) || userNumberChoice % 1 !== 0);
+
+// mostro in console e nel documento il contenuto di userChoice
+
+console.log('The user number of choice is: ', userNumberChoice);
