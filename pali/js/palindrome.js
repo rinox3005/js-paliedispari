a08fa58b21e5fa27179e285e456f443ea67be8b1
rinox3005 <peppe.rinox@gmail.com>
2024-05-07 18:11:33 +0200

    [UPD] palindrome word check done

--- pali/js/palindrome.js
+++ pali/js/palindrome.js
@@ -1,1 +1,43 @@
 'use strict';
+
+//chiedo all'utente tramite prompt una parola o una frase
+
+const userInput = prompt('Enter a word or a phrase. Is it Palindrome?');
+
+//stampo in console l'input utente
+
+console.log('Word or phrase entered by the user:', userInput);
+
+// funzione
+
+function palindromeCheck(string) {
+
+    // converto la stringa in array
+    const arrayValues = string.split('');
+
+    console.log(arrayValues);
+
+    // inverto i valori dell'array
+    const reverseArrayValues = arrayValues.reverse();
+
+    console.log(reverseArrayValues);
+
+    // riconverto l'array in stringa
+    const reverseString = reverseArrayValues.join('');
+
+    console.log(reverseString);
+
+    // controllo se la stringa invertita é uguale all'input utente
+
+    if (string === reverseString) {
+        console.log('It is a palindrome');
+    }
+    else {
+        console.log('It\'s not a palindrome');
+    }
+
+}
+
+// invocazione della funzione
+
+palindromeCheck(userInput);
